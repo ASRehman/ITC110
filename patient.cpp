b7d7cb1e76237f518583b77ead9abff92c7e5388
ASRehman <35800476+ASRehman@users.noreply.github.com>
2020-09-23 14:34:11 +0430

    Update patient.cpp

--- patient.cpp
+++ patient.cpp
@@ -1,8 +1,13 @@
 int pid;
 string pname;
-string address;
+string paddress;
 string email;
 cout<<"Enter patient name";
 cin>>pid;
 cout<<"Enter pname";
 cin>>pname;
+cout<<"Enter address";
+cin>>paddress;
+cout<<"Enter email address";
+cin>>email;
+
